a7fbdc1a1b8357e085a501466f0dc2e098ffa354
Ari <ari@Aris-Mac-mini.local>
2022-03-25 16:40:52 +0000

    Moving PTO stuff

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # This will be automated eventually- being lazy for now
-pandoc -s -c main.css -A footer.html -H main_header.html main.md -o index.html
+pandoc -s -c main.css -A footer.html -H header.html main.md -o index.html
 cd ProblemStatement/ && ./build && cd ../
 cd TommyRank/ && ./build && cd ../
 cd eloExplained/ && ./build && cd ../
-
+cd PTO_scoring && ./build && cd ../
 cd PrimoRank/ && ./build && cd ../
